4c29e53ed916e2ed6bbea80d3a72c4421509c219
Olivier Revial <olivier.revial@ext.cdiscount.com>
2017-03-22 18:43:14 +0100

    Initiate project with start.spring.io default app

---
+++ src/main/java/fr/ippon/microservices/SpringbootServiceDocumentationDemoApplication.java
@@ -0,0 +1,12 @@
+package fr.ippon.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceDocumentationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceDocumentationDemoApplication.class, args);
+	}
+}
