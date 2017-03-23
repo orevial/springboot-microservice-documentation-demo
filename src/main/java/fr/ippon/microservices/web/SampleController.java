2d619583ef8dc839e373e665cfe312e4a0be3854
Olivier Revial <olivier.revial@ext.cdiscount.com>
2017-03-23 11:46:14 +0100

    Add Swagger2 documentation

--- src/main/java/fr/ippon/microservices/web/SampleController.java
+++ src/main/java/fr/ippon/microservices/web/SampleController.java
@@ -1,17 +1,21 @@
 package fr.ippon.microservices.web;
 
-import fr.ippon.microservices.model.SearchRequest;
-import fr.ippon.microservices.model.SearchResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.ippon.microservices.model.SearchRequest;
+import fr.ippon.microservices.model.SearchResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * Sample controllers with example methods
  */
 @RestController
 @RequestMapping("/api/sample-controller")
+@Api(tags = "Sample Controller")
 public class SampleController {
 
     /**
@@ -19,6 +23,7 @@ public class SampleController {
      * @return one of the custom string settings
      */
     @RequestMapping(method = RequestMethod.GET, value = "/method1")
+    @ApiOperation(value = "Method 1", notes = "Return a sample value")
     public String method1() {
         return "This is method 1 return value";
     }
@@ -28,6 +33,7 @@ public class SampleController {
      * @return one of the custom boolean settings
      */
     @RequestMapping(method = RequestMethod.POST, value = "/search")
+    @ApiOperation(value = "Search", notes = "Returns phrases based on given keywords")
     public SearchResult search(@RequestBody SearchRequest searchRequest) {
         SearchResult searchResult = new SearchResult();
         searchRequest.getKeywords().forEach(
