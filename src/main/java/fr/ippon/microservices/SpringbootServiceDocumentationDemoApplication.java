875a547390cad04de50ee9132dbdabd98fa90a68
Olivier Revial <olivier.revial@ext.cdiscount.com>
2017-03-23 09:25:28 +0100

    Add Javadoc

--- src/main/java/fr/ippon/microservices/SpringbootServiceDocumentationDemoApplication.java
+++ src/main/java/fr/ippon/microservices/SpringbootServiceDocumentationDemoApplication.java
@@ -3,6 +3,9 @@ package fr.ippon.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application main class
+ */
 @SpringBootApplication
 public class SpringbootServiceDocumentationDemoApplication {
 
