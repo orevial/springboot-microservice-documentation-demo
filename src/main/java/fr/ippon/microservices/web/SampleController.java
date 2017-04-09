751f58fbcb30832985ef229a82754de38be3bde6
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2017-04-09 02:19:27 +0200

    Adding swagger doc

--- src/main/java/fr/ippon/microservices/web/SampleController.java
+++ src/main/java/fr/ippon/microservices/web/SampleController.java
@@ -2,6 +2,8 @@ package fr.ippon.microservices.web;
 
 import fr.ippon.microservices.model.SearchRequest;
 import fr.ippon.microservices.model.SearchResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/sample-controller")
+@Api(tags = "Sample Controller")
 public class SampleController {
 
     /**
@@ -19,6 +22,7 @@ public class SampleController {
      * @return a sample String
      */
     @RequestMapping(method = RequestMethod.GET, value = "/method1")
+    @ApiOperation(value = "Method 1", notes = "Return a sample value")
     public String method1() {
         return "This is method 1 return value";
     }
@@ -28,6 +32,7 @@ public class SampleController {
      * @return SearchResult
      */
     @RequestMapping(method = RequestMethod.POST, value = "/search")
+    @ApiOperation(value = "Search", notes = "Returns phrases based on given keywords")
     public SearchResult search(@RequestBody SearchRequest searchRequest) {
         SearchResult searchResult = new SearchResult();
         searchRequest.getKeywords().forEach(
