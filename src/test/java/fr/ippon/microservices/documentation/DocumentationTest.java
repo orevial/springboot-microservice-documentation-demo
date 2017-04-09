23f924199040476c7bd8e430755ac39932d47311
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2017-04-09 21:51:44 +0200

    Adding REST API snippets

--- src/test/java/fr/ippon/microservices/documentation/DocumentationTest.java
+++ src/test/java/fr/ippon/microservices/documentation/DocumentationTest.java
@@ -2,17 +2,22 @@ package fr.ippon.microservices.documentation;
 
 import io.github.robwin.swagger2markup.GroupBy;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import springfox.documentation.staticdocs.Swagger2MarkupResultHandler;
 
+import static org.springframework.restdocs.http.HttpDocumentation.httpRequest;
+import static org.springframework.restdocs.http.HttpDocumentation.httpResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DocumentationTest {
-
+    public static final String APIDOC_SNIPPETS_OUTPUT_FOLDER = "src/docs/asciidoc/apidoc/generated-snippets";
     public static final String APIDOC_OUTPUT_FOLDER = "src/docs/asciidoc/apidoc";
     public static final String API_DOCS_URL = "/v2/api-docs";
 
@@ -33,9 +38,16 @@ public class DocumentationTest {
 
     private MockMvc mockMvc;
 
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation(APIDOC_OUTPUT_FOLDER);
+
     @Before
     public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(restDocumentation).snippets()
+                .withDefaults(
+                        httpRequest(),
+                        httpResponse())).build();
     }
 
     @Test
@@ -45,6 +57,7 @@ public class DocumentationTest {
                 .andDo(Swagger2MarkupResultHandler
                         .outputDirectory(APIDOC_OUTPUT_FOLDER)
                         .withPathsGroupedBy(GroupBy.TAGS)
+                        .withExamples(APIDOC_SNIPPETS_OUTPUT_FOLDER)
                         .build())
                 .andExpect(status().isOk());
     }
